d1b285b02ee00f2e4226ebbee5afab8e3b1be4c7
DAMILOLA8909 <damsony.soji@gmail.com>
2025-11-07 20:51:24 +0100

    debugged Subjects.ts to setTeacher(teacher:Teacher), this.teacher = teacher

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,26 +1,11 @@
 /// <reference path="./Teacher.ts" />
 
-/**
- * Subject base class in Subjects namespace
- * Contains common functionality for all subjects
- */
 namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
-    /**
-     * Setter method for teacher
-     * @param teacher - Teacher instance to set
-     */
-    setTeacher(teacher: Teacher): void {
-      this._teacher = teacher;
-    }
-
-    /**
-     * Getter method for teacher (for internal use)
-     */
-    get teacher(): Teacher {
-      return this._teacher;
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
     }
   }
 }
